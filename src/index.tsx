1e601e5dbbb7e5c5f72b90c8fc4dd4671726dbd3
Gerard O'Neill <grardb@gmail.com>
2018-07-27 22:29:49 -0700

    Change project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import './index.css'
 
+import { App } from 'components/App'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { App } from './App'
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from 'registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
 registerServiceWorker()
