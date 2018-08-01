1cf79cc3e89335ee6f9ba92e94388daa9a60c0e9
Mark W <markwuu@gmail.com>
2018-07-31 19:06:28 -1000

    Add form component

---
+++ src/components/UI/Form/FormHeader.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const FormHeader = styled.h2`
+  font-size: 25px;
+  letter-spacing: 2px;
+  text-align: center;
+`
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Masthead } from 'components/LandingPage/Masthead'
+import { Form } from 'components/Form'
 
 storiesOf('Masthead', module).add('default', () => <Masthead />)
+
+storiesOf('Form Component', module).add('default', () => <Form />)
