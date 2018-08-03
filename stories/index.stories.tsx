70ede907b1370b1071280ef1cd669b6de001fef3
Gerard O'Neill <GRardB@users.noreply.github.com>
2018-08-02 22:15:18 -0700

    Enable TypeScript for Storybook (#7)

---
+++ stories/index.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Masthead } from 'components/LandingPage/Masthead'
+
+storiesOf('Masthead', module).add('default', () => <Masthead />)
