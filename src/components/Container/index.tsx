bfaf3f89b52fbbb57730863ea4b8cc69daff3844
Mark W <markwuu@gmail.com>
2018-08-19 23:20:59 -1000

    styled form component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  border: 1px solid black;
+  border: 2px solid #ccc;
+  border-radius: 4px;
   display: inline-block;
   margin: 0 auto;
   max-width: 1400px;
