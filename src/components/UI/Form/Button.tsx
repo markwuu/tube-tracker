44159f0f8edb10f5786f1af7dd8b5968fcba4f93
Mark W <markwuu@gmail.com>
2018-08-20 07:13:05 -1000

    WIP

--- src/components/UI/Form/Button.tsx
+++ src/components/UI/Form/Button.tsx
@@ -4,6 +4,7 @@ export const Button = styled.button`
   background-color: #fff;
   border: 2px #61bcf6;
   border-radius: 5px;
+  box-shadow: 1px 2px #888888;
   color: #fff;
   background-color: #61bcf6;
   display: inline-block;
