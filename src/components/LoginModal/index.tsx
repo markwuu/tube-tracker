b6bc896de49862557962775b2bafe605c58ba948
Mark W <markwuu@gmail.com>
2018-09-02 15:21:34 -1000

    Created simple form component

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Modal } from '../UI/Modal/index'
+import { Input } from '../UI/Form/Input'
+import { PrimaryButton } from '../UI/Buttons/index'
 
+const FormComponent = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 10px;
+
+  ${PrimaryButton} {
+    margin: 10px;
+  }
+`
 interface Props {
   onClose: () => void
 }
 
 export const LoginModal: React.SFC<Props> = ({ onClose }) => (
   <Modal title="Login" onClose={onClose}>
-    Login goes here
+    <FormComponent>
+      <Input type="text" placeholder="username" />
+      <Input type="password" placeholder="password" />
+      <PrimaryButton>Log in</PrimaryButton>
+    </FormComponent>
   </Modal>
 )
--- src/components/SignupModal/index.tsx
+++ src/components/SignupModal/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Modal } from '../UI/Modal/index'
+import { Input } from '../UI/Form/Input'
+import { PrimaryButton } from '../UI/Buttons/index'
 
+const FormComponent = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 10px;
+
+  ${PrimaryButton} {
+    margin: 10px;
+  }
+`
 interface Props {
   onClose: () => void
 }
 
 export const SignupModal: React.SFC<Props> = ({ onClose }) => (
   <Modal title="Sign Up" onClose={onClose}>
-    Sign up form goes here.
+    <FormComponent>
+      <Input type="text" placeholder="username" />
+      <Input type="password" placeholder="password" />
+      <PrimaryButton>Sign up</PrimaryButton>
+    </FormComponent>
   </Modal>
 )
