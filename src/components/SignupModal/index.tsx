4d2d21924dd4f07f3b6d8cd413eb0747e265804c
Mark W <markwuu@gmail.com>
2018-09-16 12:41:04 -1000

    Connected signup form to backend database

--- src/components/SignupModal/index.tsx
+++ src/components/SignupModal/index.tsx
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 import { Modal } from '../UI/Modal/index'
 import { Input } from '../UI/Form/Input'
 import { PrimaryButton } from '../UI/Buttons/index'
+import { Subscribe } from 'unstated'
+import { UserContainer } from 'state/user'
 
-const Form = styled.div`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
@@ -19,13 +21,24 @@ interface Props {
 }
 
 export const SignupModal: React.SFC<Props> = ({ onClose }) => (
-  <Modal title="Sign Up" onClose={onClose}>
-    <Form>
-      <label>Username</label>
-      <Input type="text" />
-      <label>Password</label>
-      <Input type="password" />
-      <PrimaryButton>Sign up</PrimaryButton>
-    </Form>
-  </Modal>
+  <Subscribe to={[UserContainer]}>
+    {(userContainer: UserContainer) => (
+      <Modal title="Sign Up" onClose={onClose}>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault()
+            const form = e.target
+            // tslint:disable-next-line
+            userContainer.signup(form['email'].value, form['password'].value)
+          }}
+        >
+          <label>Username</label>
+          <Input name="email" type="text" required />
+          <label>Password</label>
+          <Input name="password" type="password" required />
+          <PrimaryButton type="submit">Sign up</PrimaryButton>
+        </Form>
+      </Modal>
+    )}
+  </Subscribe>
 )
